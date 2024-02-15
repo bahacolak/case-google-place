2480c53cb13238d6b58edd6a8158e5bf0d1adf24
Bahadir <bahadircolak34@gmail.com>
2024-02-16 02:09:44 +0300

    first commit

---
+++ google-place-backend/src/main/java/com/bahadircolak/googleplacebackend/GooglePlaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bahadircolak.googleplacebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglePlaceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GooglePlaceBackendApplication.class, args);
+    }
+
+}
---
+++ google-place-backend/src/test/java/com/bahadircolak/googleplacebackend/GooglePlaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahadircolak.googleplacebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglePlaceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
