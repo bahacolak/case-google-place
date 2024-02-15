7078c2aec7ecf23c92e1be7b1a32d67bd04cde1e
Bahadir <bahadircolak34@gmail.com>
2024-02-16 02:58:00 +0300

    Place model class added

---
+++ google-place-backend/src/main/java/com/bahadircolak/googleplacebackend/model/Place.java
@@ -0,0 +1,26 @@
+package com.bahadircolak.googleplacebackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double longitude;
+    private double latitude;
+    private double radius;
+}
