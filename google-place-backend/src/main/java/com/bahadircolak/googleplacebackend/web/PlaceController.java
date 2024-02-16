9502c3798330207fbb0af3b26cfdd8d2a12de857
Bahadir <bahadircolak34@gmail.com>
2024-02-16 03:07:26 +0300

    Controller added

--- google-place-backend/src/main/java/com/bahadircolak/googleplacebackend/web/PlaceController.java
+++ google-place-backend/src/main/java/com/bahadircolak/googleplacebackend/web/PlaceController.java
@@ -1,4 +1,29 @@
 package com.bahadircolak.googleplacebackend.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PlaceController {
+
+    private final PlaceService placeService;
+
+    private static final Logger log = LoggerFactory.getLogger(PlaceController.class);
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @GetMapping("/places")
+    public PlacesResponse getNearbyPlaces(@RequestParam double longitude,
+                                          @RequestParam double latitude,
+                                          @RequestParam double radius) {
+
+        log.info("Received request with coordinates: {}, {}, radius: {}", longitude, latitude, radius);
+
+        return new PlacesResponse(placeService.getNearbyPlaces(longitude, latitude, radius));
+    }
 }
