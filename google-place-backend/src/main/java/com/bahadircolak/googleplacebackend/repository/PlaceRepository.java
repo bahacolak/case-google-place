13d13eaf1b08f56229de16df0887cff737cf3f9e
Bahadir <bahadircolak34@gmail.com>
2024-02-16 03:14:50 +0300

    Place repository fixed

--- google-place-backend/src/main/java/com/bahadircolak/googleplacebackend/repository/PlaceRepository.java
+++ google-place-backend/src/main/java/com/bahadircolak/googleplacebackend/repository/PlaceRepository.java
@@ -3,8 +3,12 @@ package com.bahadircolak.googleplacebackend.repository;
 import com.bahadircolak.googleplacebackend.model.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
+    List<Place> findByLongitudeAndLatitudeAndRadius(double longitude, double latitude, double radius);
 
+    Place findFirstByLongitudeAndLatitudeAndName(double longitude, double latitude, String name);
 }
